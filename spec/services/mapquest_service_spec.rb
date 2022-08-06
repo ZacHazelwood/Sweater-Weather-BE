143cc9e714b95934eb991b57cb5eca41626a2cf2
Zachary Hazelwood <zhaze001@gmail.com>
2022-08-06 14:10:39 -0600

    Test: MapQuest response contains required keys

--- spec/services/mapquest_service_spec.rb
+++ spec/services/mapquest_service_spec.rb
@@ -11,5 +11,9 @@ RSpec.describe MapquestService do
     response = MapquestService.get_location_data("denver, co")
 
     expect(response).to be_a Hash
+
+    expect(response).to have_key(:results)
+    expect(response[:results].first).to have_key(:locations)
+    expect(response[:results].first[:locations].first).to have_key(:latLng)
   end
 end
