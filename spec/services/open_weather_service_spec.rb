f233de6c00620a4b4d72b5ef0bc4a9cf363314bb
Zachary Hazelwood <zhaze001@gmail.com>
2022-08-06 15:23:28 -0600

    Test: OpenWeather Service establishes a connection

---
+++ spec/services/open_weather_service_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe OpenWeatherService do
+  it "establishes a connection" do
+    conn = OpenWeatherService.conn
+
+    expect(conn.class).to eq(Faraday::Connection)
+  end
+
+  it 'gets forecast data from a MapQuest location' do
+    data = { :latLng => {  lat: 39.738453, lng: -104.984853 } }
+    location = MapquestLocation.new(data)
+
+    expect(OpenWeatherService.get_forecast_data(location)).to be_a Hash
+  end
+end
