c45a2a5f3f9dc32c24af494871c2e9ea6c2e9352
Zachary Hazelwood <zhaze001@gmail.com>
2022-08-07 14:00:30 -0600

    Feat: Sends error if no location input

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -1,9 +1,17 @@
 class Api::V1::ForecastController < ApplicationController
   def index
-    location = MapquestFacade.get_mapquest_location_data(forecast_params)
-    forecast = OpenWeatherFacade.create_forecast_results(location)
+    if forecast_params[:location]
+      location = MapquestFacade.get_mapquest_location_data(forecast_params)
+      forecast = OpenWeatherFacade.create_forecast_results(location)
 
-    render json: ForecastSerializer.new(forecast)
+      render json: ForecastSerializer.new(forecast)
+    else
+      render json: { status: "Not Found", code: 400, message: "Please enter a location" }, status: 400
+    end
+    # location = MapquestFacade.get_mapquest_location_data(forecast_params)
+    # forecast = OpenWeatherFacade.create_forecast_results(location)
+    #
+    # render json: ForecastSerializer.new(forecast)
   end
 
   private
