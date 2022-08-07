e622112f5b71e7b81bb7b85c1ef90fc725ffc339
Zachary Hazelwood <zhaze001@gmail.com>
2022-08-07 14:11:42 -0600

    Remove comment

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -8,10 +8,6 @@ class Api::V1::ForecastController < ApplicationController
     else
       render json: { status: "Not Found", code: 400, message: "Please enter a location" }, status: 400
     end
-    # location = MapquestFacade.get_mapquest_location_data(forecast_params)
-    # forecast = OpenWeatherFacade.create_forecast_results(location)
-    #
-    # render json: ForecastSerializer.new(forecast)
   end
 
   private
