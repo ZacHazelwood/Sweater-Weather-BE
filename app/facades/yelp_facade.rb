3c05b4a0a2cb6722d80c06941fa1036894473ba8
Zachary Hazelwood <zhaze001@gmail.com>
2022-08-08 11:43:53 -0600

    Add attribute tests to Yelp/Munchie Request

--- app/facades/yelp_facade.rb
+++ app/facades/yelp_facade.rb
@@ -5,7 +5,7 @@ class YelpFacade
     forecast = OpenWeatherFacade.create_forecast_results(location)
     forecast_data = { conditions: forecast.current_weather.conditions, temperature: (forecast.current_weather.temperature.to_i.to_s + " F") }
     yelp_search = YelpService.get_business_data(location, params[:food])
-    business = { name: yelp_search[:businesses][0][:name], address: yelp_search[:businesses][0][:location][:display_address] }
+    business = { name: yelp_search[:businesses][0][:name], address: yelp_search[:businesses][0][:location][:display_address].join(", ") }
     YelpSearch.new(params[:location], forecast_data, business)
   end
 end
--- spec/requests/api/v1/forecast/yelp_requests_spec.rb
+++ spec/requests/api/v1/forecast/yelp_requests_spec.rb
@@ -3,9 +3,29 @@ require 'rails_helper'
 RSpec.describe 'Yelp Requests' do
   it "completes a request" do
     get '/api/v1/munchies?location=denver,co&food=asian'
-# require "pry"; binding.pry
+
     response_body = JSON.parse(response.body, symbolize_names: true)
 
     expect(response).to be_successful
+    expect(response.status).to eq 200
+
+    expect(response_body[:data].keys).to eq([:id, :type, :attributes])
+    expect(response_body[:data][:id]).to eq "null"
+    expect(response_body[:data][:type]).to eq "munchie"
+
+    expect(response_body[:data][:attributes].keys).to eq([:destination_city, :forecast, :restaurant])
+    expect(response_body[:data][:attributes][:destination_city]).to be_a String
+    expect(response_body[:data][:attributes][:forecast]).to be_a Hash
+    expect(response_body[:data][:attributes][:restaurant]).to be_a Hash
+
+    expect(response_body[:data][:attributes][:forecast].keys).to eq([:summary, :temperature])
+    expect(response_body[:data][:attributes][:forecast]).to_not have_key(:wind_speed)
+    expect(response_body[:data][:attributes][:forecast][:summary]).to be_a String
+    expect(response_body[:data][:attributes][:forecast][:temperature]).to be_a String
+
+    expect(response_body[:data][:attributes][:restaurant].keys).to eq([:name, :address])
+    expect(response_body[:data][:attributes][:restaurant]).to_not have_key(:review_count)
+    expect(response_body[:data][:attributes][:restaurant][:name]).to be_a String
+    expect(response_body[:data][:attributes][:restaurant][:address]).to be_a String
   end
 end
