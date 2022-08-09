28e42c64ec975acfd481c0175cecb3934481dec2
Zachary Hazelwood <zhaze001@gmail.com>
2022-08-09 11:08:54 -0600

    Feat: MapquestFacade gets travel time from two locations, or error

--- app/facades/mapquest_facade.rb
+++ app/facades/mapquest_facade.rb
@@ -4,4 +4,13 @@ class MapquestFacade
     result = json[:results][0][:locations][0][:latLng]
     MapquestLocation.new(result)
   end
+
+  def self.get_travel_time(from, to)
+    directions_json = MapquestService.get_directions_data(from, to)
+    if directions_json[:info][:messages].include?("We are unable to route with the given locations.")
+      "impossible route"
+    else
+      directions_json[:route][:formattedTime]
+    end
+  end
 end
