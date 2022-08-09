b43b4bc15409418c9cffa1cfd1e71a91efc2fcd2
Zachary Hazelwood <zhaze001@gmail.com>
2022-08-09 10:55:16 -0600

    Feat: MapQuest directions connections, add test for keys

--- app/services/mapquest_service.rb
+++ app/services/mapquest_service.rb
@@ -12,4 +12,14 @@ class MapquestService
     url = 'http://www.mapquestapi.com'
     Faraday.new(url: url)
   end
+
+  def self.get_directions_data(from, to)
+    end_point = '/directions/v2/route'
+    response = conn.get(end_point) do |faraday|
+      faraday.params['from'] = from
+      faraday.params['to'] = to
+      faraday.params['key'] = ENV['mapquest_api_key']
+    end
+    JSON.parse(response.body, symbolize_names: true)
+  end
 end
