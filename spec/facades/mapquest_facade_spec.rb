1b9db7f8993753f6207da593394e3dda74e55901
Zachary Hazelwood <zhaze001@gmail.com>
2022-08-09 11:08:32 -0600

    Test: MapquestFacade happy/sad paths for travel time

--- spec/facades/mapquest_facade_spec.rb
+++ spec/facades/mapquest_facade_spec.rb
@@ -1,11 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe MapquestFacade do
-  it "gets location data" do
-    location = MapquestFacade.get_mapquest_location_data('denver, co')
+  describe 'happy path' do
+    it "creates location object" do
+      location = MapquestFacade.get_mapquest_location_data('denver, co')
 
-    expect(location).to be_a MapquestLocation
-    expect(location.latitude).to eq(39.738453)
-    expect(location.longitude).to eq(-104.984853)
+      expect(location).to be_a MapquestLocation
+      expect(location.latitude).to eq(39.738453)
+      expect(location.longitude).to eq(-104.984853)
+    end
+
+    it "gets the travel time between two locations" do
+      travel_time = MapquestFacade.get_travel_time("denver, co", "boulder, co")
+
+      expect(travel_time).to be_a String
+    end
+  end
+
+  describe 'sad path' do
+    it "cannot provide time of an impossible route" do
+      travel_time = MapquestFacade.get_travel_time("sarasota, fl", "london, england")
+
+      expect(travel_time).to be_a String
+      expect(travel_time).to eq("impossible route")
+    end
   end
 end
